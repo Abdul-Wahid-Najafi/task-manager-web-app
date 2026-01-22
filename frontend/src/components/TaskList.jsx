42393ba13733d5a22430b9401fecc583725401d3
wahid-git200 <abdullwahidnajafi@gmail.com>
2026-01-22 20:24:44 +0430

    add task added

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import TaskItem from './TaskItem';
+import React from "react";
+import TaskItem from "./TaskItem";
 
 const TaskList = ({ tasks }) => {
   return (
-    <div className='flex flex-col gap-3 mb-6 overflow-y-auto'>
-      {tasks.map(task => (
+    <div className="flex flex-col gap-3 mb-6 overflow-y-auto">
+      {tasks.map((task) => (
         <TaskItem key={task.id} task={task} />
       ))}
     </div>
